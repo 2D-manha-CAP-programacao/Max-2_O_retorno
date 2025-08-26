cd4d8e458c17fe1081b6270c7f94cde3426380d6
Maxwell <maxwell.barreto.santos@escola.pr.gov.br>
2025-08-26 21:54:26 +0000

    oie

---
+++ perguntas.js
@@ -0,0 +1,29 @@
+criaCartao(
+    'Terror Cult',
+    'Qual é o nome do livro amaldiçoado presente em Evil Dead?',
+    'Necronomicon Ex-Mortis'
+);
+
+criaCartao(
+    'Terror Cult',
+    'Qual é o bordão do vilão Jigsaw em Jogos Mortais?',
+    '“I want to play a game” '
+);
+
+criaCartao(
+    'Terror Clássico',
+    'Quem é o assassino mascarado da franquia Halloween?',
+    'Michael Myers'
+);
+
+criaCartao(
+    'Terror Moderno',
+    'Qual filme de terror popularizou a filmagem estilo found footage em 1999?',
+    'A Bruxa de Blair '
+);
+
+criaCartao(
+    'Slasher',
+    'Qual o nome do assassino com luva de lâminas nos sonhos?',
+    'Freddy Krueger'
+);
